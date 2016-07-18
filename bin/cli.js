d788f97338d9160f8cb7b4079600fc978495182f
Nicolás Fantone <nfantone@contentamobile.com>
2016-07-18 18:54:38 -0300

    Run npm init and install

--- bin/cli.js
+++ bin/cli.js
@@ -3,8 +3,9 @@
 'use strict';
 /**
  * A command line utility to generate
- * a Koa 2.0.0 REST API scaffolding.
+ * a Koa 2.0.0+ REST API scaffolding.
  */
+const spawn = require('child-process-promise').spawn;
 const path = require('path');
 const argv = require('yargs')
   .usage('Usage: mu-koan [name]')
@@ -40,24 +41,52 @@ function exit(err, level) {
   return process.exit();
 }
 
+/**
+ * Install all npm dependencies declared on package.json.
+ * Redirects npm's stderr to this process' stderr.
+ *
+ * @method installDependencies
+ * @return {Stream}
+ */
+function installDependencies() {
+  log.info('Installing npm dependencies (this may take a while)');
+  return spawn('npm', ['install'], {
+    stdout: 'inherit',
+    stderr: 'inherit'
+  });
+}
+
+function runNpmInit() {
+  log.debug('Initializing npm project');
+  return spawn('npm', ['init'], {
+    stdio: 'inherit',
+    stdout: 'inherit'
+  });
+}
+
 function main() {
   var destinationPath = process.cwd() || '.';
 
+  // Check if CWD is not empty
   emptyDir(destinationPath, (err, result) => {
     if (err) {
       return exit(err);
     }
     if (result) {
       log.info('Creating project "%s" into [%s]', argv.name, destinationPath);
+
+      // Copy everything under ./templates to CWD
       cpr(TEMPLATES_DIR, destinationPath, {
-        confirm: true,
-        filter: /package[.]json$/
+        confirm: true
       }, (err, files) => {
         if (err) {
           return exit(err);
         }
         files.forEach((file) => log.info('Created %s', file));
-        log.info('Done');
+        runNpmInit()
+          .then(() => installDependencies())
+          .then(() => log.info('Done'))
+          .catch(log.error);
       });
     } else {
       return exit(`destination [${destinationPath}] is not empty`, 'warn');
